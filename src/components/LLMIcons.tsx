bb3212711a921f1bd004f9300d14426c3bfd116b
Zhang Minghan <zmh13054618081@dingtalk.com>
2024-07-15 10:46:50 +0800

    pro

--- src/components/LLMIcons.tsx
+++ src/components/LLMIcons.tsx
@@ -5,16 +5,21 @@ import {
   Baichuan,
   ByteDance,
   Claude,
-  Dalle, DeepSeek,
-  Gemini, Groq,
+  Dalle,
+  DeepSeek,
+  Gemini,
+  Groq,
   Hunyuan,
-  Meta, Midjourney, Moonshot,
-  OpenAI, OpenRouter,
+  Meta,
+  Midjourney,
+  Moonshot,
+  OpenAI,
+  OpenRouter,
   Qingyan,
-  Spark, Stability,
-  Tongyi
+  Spark,
+  Stability,
+  Tongyi,
 } from "@lobehub/icons";
-
 
 const icons = [
   Dalle,
@@ -38,12 +43,12 @@ const icons = [
 
 export default function LLMIcons() {
   return (
-  <div className={`grid grid-cols-7 mt-3 gap-2 w-full`}>
-    <OpenAI.Avatar size={28} type={`gpt3`}/>
-    <OpenAI.Avatar size={28} type={`gpt4`}/>
-    {icons.map((Icon, index) => (
-      <Icon.Avatar key={index} size={28}/>
-    ))}
-  </div>
-  )
+    <div className={`grid grid-cols-7 mt-3 gap-2 w-full`}>
+      <OpenAI.Avatar size={28} type={`gpt3`} />
+      <OpenAI.Avatar size={28} type={`gpt4`} />
+      {icons.map((Icon, index) => (
+        <Icon.Avatar key={index} size={28} />
+      ))}
+    </div>
+  );
 }
--- src/pages/pro.tsx
+++ src/pages/pro.tsx
@@ -1,9 +1,10 @@
 import { Inter } from "next/font/google";
-import {Check} from "lucide-react";
+import { Check } from "lucide-react";
 import dynamic from "next/dynamic";
 
-
-const LLMIcons = dynamic(() => import(`../components/LLMIcons`), { ssr: false });
+const LLMIcons = dynamic(() => import(`../components/LLMIcons`), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,7 @@ export default function Home() {
         className={`w-screen h-screen select-none flex items-center justify-center md:p-20`}
       >
         <div
-          className={`w-full h-full flex flex-row p-4 items-center rounded-lg shadow-lg bg-background border relative`}
+          className={`w-full h-fit flex flex-row p-4 items-center rounded-lg shadow-lg bg-background border relative`}
           style={{
             borderColor: `hsl(var(--border-hover))`,
           }}
@@ -24,35 +25,61 @@ export default function Home() {
           >
             <div className={`relative w-28 h-28`}>
               <img src={`/logo.png`} alt={``}/>
-              <div className={`absolute w-full h-full`} style={{
-                position: `absolute`,
-                top: `50%`,
-                left: `50%`,
-                borderRadius: `50%`,
-                backgroundImage: `linear-gradient( -45deg, rgba(0, 126, 255, .2) 50%, rgba(73, 164, 255, .2) )`,
-                filter: `blur(20px)`,
-                transform: `translate(-50%, -50%)`,
-              }}/>
+              <div
+                className={`absolute w-full h-full`}
+                style={{
+                  position: `absolute`,
+                  top: `50%`,
+                  left: `50%`,
+                  borderRadius: `50%`,
+                  backgroundImage: `linear-gradient( -45deg, rgba(0, 126, 255, .2) 50%, rgba(73, 164, 255, .2) )`,
+                  filter: `blur(20px)`,
+                  transform: `translate(-50%, -50%)`,
+                }}
+              />
             </div>
-            <h1 className={`relative text-3xl flex flex-row items-center whitespace-nowrap`}>
-              Chat Nio
+            <h1
+              className={`relative text-3xl flex flex-row items-center whitespace-nowrap`}
+            >
+              <p style={{
+                // text gradient: sky -> white ->  gold
+                backgroundImage: `linear-gradient( 45deg, rgba(0, 146, 255) 40%, rgba(255, 255, 255) 50%, rgba(225, 165, 0) )`,
+                WebkitBackgroundClip: `text`,
+                WebkitTextFillColor: `transparent`,
+              }}>Chat Nio</p>
               <p
-                className={`text-white rounded-md px-2 ml-2 py-1 text-xs bg-gradient-to-r from-amber-400 to-amber-600`}>
+                className={`text-white rounded-md px-2 ml-2 py-1 text-xs bg-gradient-to-r from-amber-400 to-amber-600`}
+              >
                 PRO
               </p>
             </h1>
             <p className={`text-sm mt-2 text-center whitespace-nowrap`}>
               ⚡ Enterprise Grade Solution
             </p>
-            <img className={`mt-1 mb-4 w-1/2`} src={`https://skillicons.dev/icons?i=kubernetes,rust,mysql,redis`}
-                 alt={``}/>
+            <img
+              className={`mt-2 mb-3 w-1/2`}
+              src={`https://skillicons.dev/icons?i=kubernetes,rust,mysql,redis`}
+              alt={``}
+            />
 
-            <div className={`flex flex-col items-center flex-wrap space-y-1 mt-4 text-sm`}>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+            <div
+              className={`flex flex-col items-center flex-wrap space-y-1 mt-4 text-sm`}
+              style={{
+                // gold text gradient
+                backgroundImage: `linear-gradient( 45deg, rgba(255, 215, 0) 50%, rgba(255, 80, 0) )`,
+                WebkitBackgroundClip: `text`,
+                WebkitTextFillColor: `transparent`,
+              }}
+            >
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 TTS & STT
               </div>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 Plugin System
               </div>
@@ -60,39 +87,75 @@ export default function Home() {
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 Open GPTs Platform
               </div>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 Multi Payment Providers
               </div>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 Enterprise Channel Manager
               </div>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 Analytics & Insights Dashboard
               </div>
-              <div className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}>
+              <div
+                className={`flex flex-row items-center text-amber-500 whitespace-nowrap`}
+              >
                 <Check className={`w-3.5 h-3.5 mr-1`}/>
                 ...
               </div>
             </div>
           </div>
-          <div className={`absolute text-4xl right-4 top-4`}>
-            😎
-          </div>
           <div className={`h-full flex-grow flex ml-6 p-6 z-10`}>
-            <div className={`h-full grid grid-cols-2 gap-2.5`}>
-              <img src={`/screenshot/vision.png`} alt={``} className={`border p-0.5 bg-background shadow rounded-md`}/>
-              <img src={`/screenshot/editor.png`} alt={``} className={`border p-0.5 bg-background shadow rounded-md`}/>
-              <img src={`/screenshot/admin.png`} alt={``} className={`border p-0.5 bg-background shadow rounded-md`}/>
-              <img src={`/screenshot/mask-editor.png`} alt={``} className={`border p-0.5 bg-background shadow rounded-md`}/>
+            <div className={`h-full grid grid-cols-3 gap-2.5`}>
+              <img
+                src={`/screenshot/pro-market.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
+              <img
+                src={`/screenshot/pro-agent.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
+              <img
+                src={`/screenshot/pro-account.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
+              <img
+                src={`/screenshot/pro-admin.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
+              <img
+                src={`/screenshot/pro-channel.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
+              <img
+                src={`/screenshot/pro-wallet.png`}
+                alt={``}
+                className={`border p-0.5 bg-background shadow rounded-md`}
+              />
             </div>
           </div>
-          <div className={`absolute w-64 h-64 right-0 top-0`} style={{
-            backgroundImage: `linear-gradient( 45deg, rgba(255, 215, 0, .2) 50%, rgba(255, 205, 0, .3) )`,
-            filter: `blur(50px)`,
-          }}/>
+          <div
+            className={`absolute w-64 h-64 right-0 top-0`}
+            style={{
+              backgroundImage: `linear-gradient( 45deg, rgba(255, 215, 0, .2) 50%, rgba(255, 205, 0, .3) )`,
+              filter: `blur(50px)`,
+            }}
+          />
+          <div className={`absolute text-4xl right-4 top-4 rotate-1`}>😎</div>
+          <div className={`absolute text-2xl left-5 bottom-4 opacity-40`}>🥳</div>
         </div>
       </div>
     </main>
